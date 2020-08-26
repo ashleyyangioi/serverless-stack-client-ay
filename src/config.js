3470bc9bbde0700e53faaea53bcc287553c8db29
Ashley Yang <ashley.yang@interoperabilityinstitute.org>
2020-08-26 14:31:42 -0400

    Setting up react app with aws

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "notes-app-uploads-ay"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://as0y72ga0m.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_Ru0cFPtn3",
+        APP_CLIENT_ID: "7ecrjo90amuogbpf1b6v8ese3a",
+        IDENTITY_POOL_ID: "us-east-2:2a6adc64-93ad-46f0-827f-484c5964a936"
+    }
+};
